125d443b1d9350d64cd6697f040670eafddffe45
klicperajo <jklicpera@live.at>
2015-07-16 22:59:13 +0200

    Removed unnecessary files.

--- gen_mesh/simmodeler_script/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mpiCC create_mesh.cpp -fopenmp -o create_mesh -I/work/klicpera/simmodeler/include -L/work/klicpera/simmodeler/lib/x64_rhel5_gcc41 -lSimMeshing -lSimDiscrete -lSimMeshTools -lSimModel
